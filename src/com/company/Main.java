97fbd72956b24fa22daa7d0242dd3d2b4d376625
LanaGA <l.galieva@innopolis.ru>
2020-08-25 17:46:22 +0300

    added MyLinkedList

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        MyLinkedList<Integer> mouse = new MyLinkedList();
+        mouse.add(10);
+        mouse.add(15);
+        System.out.println(Arrays.toString(mouse.toArray()));
+        System.out.print(mouse.toString());
+        mouse.remove((Integer) 10);
+        System.out.println(mouse.toString());
+        mouse.add(10);
+        mouse.add(15);
+        mouse.set(1, 2);
+        for(Object iter: mouse){
+            System.out.println(iter);
+        }
     }
 }
